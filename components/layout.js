fe13233f2068357347fd68b4616242e21ae5f13d
최지훈 <choijh@eksys.co.kr>
2025-04-01 11:01:24 +0900

    Analytics, SpeedInsights 위치 변경

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,14 @@
 import Header from "./header"
 import Footer from "./footer"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout({ children }) {
     return (
         <div className="bg-primary">
             <Header/>
+            <Analytics />
+            <SpeedInsights />
             <div>{children}</div>
             <Footer/>
         </div>
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -8,9 +6,7 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <NextScript />
-        <Analytics />
-        <SpeedInsights />
+        <NextScript />  
       </body>
     </Html>
   )
